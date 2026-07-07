67fe79bd9d1b545bba7948b9ec955b8724b42d99
BrianHS <158072051+BrianHS@users.noreply.github.com>
2026-07-07 17:27:34 -0400

    Función leer opción

--- app.py
+++ app.py
@@ -9,3 +9,18 @@ def mostrar_menu():
       6. Mostrar productos
       7. Salir
     ===========================''')
+
+def leer_opcion():
+    while True:
+        try:
+            opcion = int(input("Seleccione una opción: "))
+
+            if opcion >= 1 and opcion <= 7:
+                return opcion
+            else:
+                print("Debe seleccionar una opción válida")
+
+        except ValueError:
+            print("Debe seleccionar una opción válida")
+
+

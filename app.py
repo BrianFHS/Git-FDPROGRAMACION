7ee48069ce08ac95cfd91c448b903102f6d93c6b
BrianHS <158072051+BrianHS@users.noreply.github.com>
2026-07-07 18:12:14 -0400

    Edición menú

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ def mostrar_menu():
     print('''
     ====== MENU PRINCIPAL ======
       1. Stock por categoría
-      2. B2. Buscar productos por rango de precio
+      2. Buscar productos por rango de precio
       3. Actualizar precio
       4. Agregar producto
       5. Eliminar producto
